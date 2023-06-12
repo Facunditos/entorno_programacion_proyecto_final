cc425ef2a623ad6378ffdcc6a6cd413b77d6ed07
Facunditos <facundolopezcrespo302@gmail.com>
2023-06-12 12:31:57 -0300

    simplifiqué script generar

--- generar.sh
+++ generar.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #LINK_NOMBRES=https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv
 #wget $LINK_NOMBRES
+cambiarNombreFoto(){
+	LISTADO_NOMBRES_CANTIDAD=`wc -l dict.csv | cut -d " " -f 1`
+	LISTADO_NOMBRES_RANDOM_LINEA=$((1+$RANDOM%$LISTADO_NOMBRES_CANTIDAD))p
+	RANDOM_NOMBRE=`sed -n $LISTADO_NOMBRES_RANDOM_LINEA dict.csv | cut -d "," -f 1 | tr " " "_"`
+	mv $1 $RANDOM_NOMBRE 	
+}	
 descargarFotos(){
 	for I in `seq 1 $2`;do
 		wget $1
+		FOTO=`find -name "index.html?person"`
+		cambiarNombreFoto $FOTO
 		sleep 5
 	done
 }	
 LINK_FOTOS=https://source.unsplash.com/random/900×700/?person
 CANTIDAD=$1
 descargarFotos $LINK_FOTOS $CANTIDAD
-cambiarNombreFoto(){
-	LISTADO_NOMBRES_CANTIDAD=`wc -l dict.csv | cut -d " " -f 1`
-	LISTADO_NOMBRES_RANDOM_LINEA=$((1+$RANDOM%$LISTADO_NOMBRES_CANTIDAD))p
-	RANDOM_NOMBRE=`sed -n $LISTADO_NOMBRES_RANDOM_LINEA dict.csv | cut -d "," -f 1 | tr " " "_"`
-	mv $1 $RANDOM_NOMBRE 	
-}
-LISTADO_FOTOS=`find -name "index.html?person*"`
-for FOTO in $LISTADO_FOTOS;do
-	cambiarNombreFoto $FOTO
-done	
 exit 0
