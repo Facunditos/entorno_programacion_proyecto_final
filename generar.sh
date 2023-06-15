7d96af02503983aa5d693f5c2056c30977d462ac
Facunditos <facundolopezcrespo302@gmail.com>
2023-06-15 16:16:01 -0300

    corregí script generar.sh para comprimir las imágenes descargadas

--- generar.sh
+++ generar.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 #LINK_NOMBRES=https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv
 #wget $LINK_NOMBRES
-cambiarNombreFoto(){
-	LISTADO_NOMBRES_CANTIDAD=`wc -l dict.csv | cut -d " " -f 1`
-	LISTADO_NOMBRES_RANDOM_LINEA=$((1+$RANDOM%$LISTADO_NOMBRES_CANTIDAD))p
-	FOTO_RANDOM_NOMBRE=`sed -n $LISTADO_NOMBRES_RANDOM_LINEA dict.csv | cut -d "," -f 1 | tr " " "_"`
-	mv $1 $FOTO_RANDOM_NOMBRE 	
+cambiarNombreImagen(){
+	LISTADO_NOMBRES_CANTIDAD=`wc -l dict.csv | cut -d " " -f 1`;
+	LISTADO_NOMBRES_RANDOM_LINEA=$((1+$RANDOM%$LISTADO_NOMBRES_CANTIDAD))p;
+	IMAGEN_RANDOM_NOMBRE=`sed -n $LISTADO_NOMBRES_RANDOM_LINEA dict.csv | cut -d "," -f 1 | tr " " "_"`;
+	mv $1 $IMAGEN_RANDOM_NOMBRE; 	
 }	
-comprimirFoto(){
-	jpegoptim $FOTO_RANDOM_NOMBRE
+moverImagen(){
+	mv $IMAGEN_RANDOM_NOMBRE ./lote_imagenes;
+}	
+comprimirImagenes(){
+	zip -j lote_imagenes ./lote_imagenes/*;
 }	
 obtenerSumaVerificacion(){
-	sha256sum $FOTO_RANDOM_NOMBRE >> sumaVerificacion
+	touch suma_verificacion;
+	sha256sum lote_imagenes.zip > suma_verificacion;
 }	
-moverFoto(){
-	mv $FOTO_RANDOM_NOMBRE ./imagenes_comprimidas
-}	
-descargarFotos(){
+descargarImagenes(){
+	mkdir lote_imagenes;
 	for I in `seq 1 $2`;do
-		wget $1
-		FOTO=`find -name "index.html?person"`
-		cambiarNombreFoto $FOTO
-		comprimirFoto 
-		obtenerSumaVerificacion 
-		moverFoto 
-		sleep 5
+		wget $1;
+		IMAGEN=`find -name "index.html?person"`;
+		cambiarNombreImagen $IMAGEN;
+		moverImagen; 
+		sleep 5;
 	done
+	comprimirImagenes;
+	obtenerSumaVerificacion;
+       rm -r lote_imagenes;	
 }	
-LINK_FOTOS=https://source.unsplash.com/random/900×700/?person
-CANTIDAD=$1
-descargarFotos $LINK_FOTOS $CANTIDAD
+LINK_IMAGENES=https://source.unsplash.com/random/900×700/?person;
+CANTIDAD=$1;
+descargarImagenes $LINK_IMAGENES $CANTIDAD;
 exit 0
