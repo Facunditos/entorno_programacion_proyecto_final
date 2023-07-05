d58c045ebe753b60127ce00f5a0636339ae03f02
Facunditos <facundolopezcrespo302@gmail.com>
2023-07-05 14:24:22 -0300

    agregué al script comprimir la funcionalidad para crear el listado de nombres válidos de imágenes

--- comprimir.sh
+++ comprimir.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 crearListadoNombresImagenes(){
-	ls lote_imagenes >> listadoNombresImagenes;
+	ls lote_imagenes > listadoNombresImagenes;
 }
-crearListadoNombresImagenes;
+crearListadoNombresValidosImagenes(){
+	for IMAGEN in `ls ./lote_imagenes`;do
+		RESOLUCION_IMAGEN=`file ./lote_imagenes/$IMAGEN | cut -d "," -f "8"`;
+		RESOLUCION_IMAGEN_NOMBRE_VALIDO="512x512"
+		if [ $RESOLUCION_IMAGEN = $RESOLUCION_IMAGEN_NOMBRE_VALIDO ];then
+			echo $IMAGEN >> listadoNombresValidosImagenes;
+		fi;
+	done;	
+}	
+#crearListadoNombresImagenes;
+crearListadoNombresValidosImagenes;
 exit 0
