0c2ca929a356589493cf2f45f3d79dc9e0432479
Facunditos <facundolopezcrespo302@gmail.com>
2023-07-05 16:50:27 -0300

    modifiqué script descomprimir

--- descomprimir.sh
+++ descomprimir.sh
@@ -3,17 +3,17 @@ moverImagenesDescomprimidas(){
 	ARCHIVOS_JPEG=`find . -type f -exec file {} \; | grep "JPEG" | cut -d ":" -f 1`;
 	mkdir lote_imagenes;
 	mv $ARCHIVOS_JPEG ./lote_imagenes;
-	echo "las imágenes se descomprimieron y fueron guardadas en el directorio lote_imagenes";
 }
 descomprimirImagenes(){
 	if [ -f *.zip ];then
 		unzip *.zip;
-		moverImagenesDescomprimidas;
-		rm *.zip;
 	else	
 		echo "No se encontró el lote de imágenes, por ende, no se pudo descomprimir";
 		exit 1;
 	fi;	
 }
-descomprimirImagenes
+descomprimirImagenes;
+moverImagenesDescomprimidas;	
+rm *.zip;
+echo "las imágenes se descomprimieron y fueron guardadas en el directorio lote_imagenes, se procedió a eliminar el archivo comprimido";
 exit 0;
