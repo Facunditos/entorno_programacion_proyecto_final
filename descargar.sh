de8e7579cda4882bad5e9d9a1f8090c2d47b878d
Facunditos <facundolopezcrespo302@gmail.com>
2023-07-06 18:44:39 -0300

    scrip descargar: se sumó como variables los archivos a eliminar si no se corroborra la integridad del archivo descargado

--- descargar.sh
+++ descargar.sh
@@ -9,7 +9,7 @@ comprobarIntegridadLoteImagenesDescargado(){
     SUMA_VERIFICACION_LOTE_IMAGENES_DESCARGADO=`sha256sum $ARCHIVO_LOTE_IMAGENES`;
     SUMA_VERIFICACION_PROVISTA_LOTE_IMAGENES_DESCARGADO=`cat $ARCHIVO_SUMA_VERIFICACION_LOTE_IMAGENES`;
     if [ "$SUMA_VERIFICACION_LOTE_IMAGENES_DESCARGADO" != "$SUMA_VERIFICACION_PROVISTA_LOTE_IMAGENES_DESCARGADO" ];then
-        rm lote_imagenes.zip suma_verificacion.sha256;
+        rm $ARCHIVO_LOTE_IMAGENES $ARCHIVO_SUMA_VERIFICACION_LOTE_IMAGENES;
         echo "no pudo verificarse la integridad del lote de imágenes, por lo cual, se procedió a eliminarlo";
         exit 1;
     else
@@ -24,7 +24,7 @@ descargarArchivos(){
 
 URL_LOTE_IMAGENES=$1;
 URL_SUMA_VERIFICACION_LOTE_IMAGENES=$2;
-descargarArchivos $1 $2;
+#descargarArchivos $1 $2;
 obtenerNombreArchivosDescargados $1 $2
 comprobarIntegridadLoteImagenesDescargado $1 $2;
 exit 0
