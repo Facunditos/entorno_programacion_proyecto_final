8d9c4d94232b4316356b8ce00cb8ad132a6cc71d
cattaino <dantecattaino@gmail.com>
2023-07-12 01:28:13 +0000

    Descomprime lote_imagenes.zip y almacena el contenido en una carpeta

--- comprimir.sh
+++ comprimir.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+unzip lote_imagenes.zip -d lote_imagenes
 crearListadoNombresImagenes(){
 	ls lote_imagenes > listadoNombresImagenes;
 }
 crearListadoNombresValidosImagenes(){
 	touch listadoNombresValidosImagenes;
 	for IMAGEN in `ls ./lote_imagenes`;do
-		RESOLUCION_IMAGEN=`file ./lote_imagenes/$IMAGEN | cut -d "," -f "8"`;
+RESOLUCION_IMAGEN=`file ./lote_imagenes/$IMAGEN | cut -d "," -f "8"`;
 		RESOLUCION_IMAGEN_NOMBRE_VALIDO="512x512"
-		if [ $RESOLUCION_IMAGEN = $RESOLUCION_IMAGEN_NOMBRE_VALIDO ];then
-			echo $IMAGEN >> listadoNombresValidosImagenes;
+if [ $RESOLUCION_IMAGEN = $RESOLUCION_IMAGEN_NOMBRE_VALIDO ];then
+		echo $IMAGEN >> listadoNombresValidosImagenes;
 		fi;
 	done;	
 }	
@@ -17,13 +18,13 @@ crearListadoNombresImagenesFinalizan_a(){
 	for IMAGEN in `ls ./lote_imagenes`;do
 		FINALIZA_a=`echo $IMAGEN | grep "a$"`;
 		if [ $FINALIZA_a ];then
-			echo $IMAGEN >> listadoNombresImagenesFinalizan_a;
+		echo $IMAGEN >> listadoNombresImagenesFinalizan_a;
 		fi;
 	done;	
 }	
 comprimirArchivos(){
-	zip -j -r archivo ./{listadoNombresImagenes,listadoNombresValidosImagenes,listadoNombresImagenesFinalizan_a,lote_imagenes/*};
-	rm -r ./{listadoNombresImagenes,listadoNombresValidosImagenes,listadoNombresImagenesFinalizan_a,lote_imagenes}
+zip -j -r archivo ./{listadoNombresImagenes,listadoNombresValidosImagenes,listadoNombresImagenesFinalizan_a,lote_imagenes/*};
+rm -r ./{listadoNombresImagenes,listadoNombresValidosImagenes,listadoNombresImagenesFinalizan_a,lote_imagenes}
 	mv archivo.zip ./carpetaCompartida;
 }
 crearListadoNombresImagenes;
